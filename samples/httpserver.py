0630de999a5b334dc751b79dcc160e96ebd6014c
ben lu <>
2019-03-26 17:54:02 +0800

    no message

--- samples/httpserver.py
+++ samples/httpserver.py
@@ -14,15 +14,17 @@ class S(BaseHTTPRequestHandler):
         self._set_headers()
         print("aaaa")
         # parsed_path = urlparse.urlparse(self.path)
-        self.wfile.write(self.blackList)
+        if len(self.blackList) > 0:
+            print(self.blackList[0].decode("utf-8"))
+        self.wfile.write(bytes(str(self.blackList), "utf-8"))
 
     def do_POST(self):
         self._set_headers()
-        content_len = int(self.headers.getheader('content-length', 0))
-        # content_len = int(self.headers.get('Content-Length'))
+        # content_len = int(self.headers.getheader('content-length', 0))
+        content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
         self.blackList.append(post_body)
-        self.wfile.write(test)
+        self.wfile.write(bytes(test, "utf-8"))
 
     def do_HEAD(self):
         self._set_headers()
